b96c79b3a0ff85b331a93724559f88fafd0c862f
Saman <saman12382usp@gmail.com>
2026-06-30 16:46:43 +0330

    feat: add monitor loop and remove prototype main script

---
+++ battery_notifier/monitor.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import time, logging, datetime
+from .battery import Battery
+from .player import Player
+from .notifier import Notifier
+
+log = logging.getLogger(__name__)
+
+class Monitor:
+    def __init__(self, cfg):
+        self.cfg = cfg
+        self.battery = Battery()
+        self.player = Player(cfg.music_files, cfg.volume, cfg.annoying)
+        self.notifier = Notifier()
+
+    def run(self) -> None:
+        log.info("Monitoring started.")
+        try:
+            while True:
+                info = self.battery.read()
+                in_target = (self.cfg.min_percentage <= info.percentage <= self.cfg.max_percentage)
+
+                if info.charging and in_target and not self.player.playing:
+                    if self.player.play():
+                        self.notifier.send("Battery target reached", f"{info.percentage}% reached.")
+                elif (not info.charging or info.percentage < self.cfg.min_percentage) and self.player.playing:
+                    self.player.stop()
+                    self.notifier.send("Stopped", "Conditions no longer met.")
+
+                time.sleep(self.cfg.poll_interval)
+        except KeyboardInterrupt:
+            log.info("Interrupted; cleaning up.")
+            self.player.stop()
